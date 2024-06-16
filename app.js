9f15026b1fb15d5ecd810000d05a366a118c2f27
Dahlia Xie <dahlia.xie@wsu.edu>
2024-06-15 17:39:51 -0700

    recover index.html

---
+++ app.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import FooterBanner from './components/FooterBanner';
+import About from './pages/About';
+import './styles/index.css';
+import Home from './pages/Home';
+import Experience from './pages/Experience';
+import Contact from './pages/Contact'
+import Sample from './pages/Sample'
+import Test from './pages/Test.js';
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/v1" element={<Sample />} />
+          <Route path="/v2" element={<Test />} />
+          <Route path="/" element={<Home />} />
+          {/* Add more routes for other pages */}
+        </Routes>
+
+
+        <FooterBanner />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
